81f2c3497d4ec6e77d031a53c5693f9ca9715353
lacie-life <00sao00ios00@gmail.com>
2021-07-09 17:56:10 +0700

    create iSAM2 Optimizer for Backend

---
+++ include/vi_slam/optimization/isam2optimizer.h
@@ -0,0 +1,20 @@
+//
+// Created by lacie on 09/07/2021.
+//
+
+#ifndef VI_SLAM_ISAM2OPTIMIZER_H
+#define VI_SLAM_ISAM2OPTIMIZER_H
+
+#include "vi_slam/common_include.h"
+
+namespace vi_slam{
+    namespace optimization{
+        class iSAM2Optimizer {
+
+        };
+    }
+}
+
+
+
+#endif //VI_SLAM_ISAM2OPTIMIZER_H
---
+++ src/optimization/isam2optimizer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by lacie on 09/07/2021.
+//
+
+#include "vi_slam/optimization/isam2optimizer.h"
+
+namespace vi_slam{
+    namespace optimization{
+        class iSAM2Optimizer {
+
+        };
+    }
+}
