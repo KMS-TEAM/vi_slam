af3405da7b50dc5a6983325fce9dba18bae63313
lacie-life <00sao00ios00@gmail.com>
2021-07-11 14:27:26 +0700

    update iSAM2 Optimizer

---
+++ include/vi_slam/optimization/iSAM2Optimizer.h
@@ -0,0 +1,16 @@
+//
+// Created by cit-industry on 11/07/2021.
+//
+
+#ifndef VI_SLAM_ISAM2OPTIMIZER_H
+#define VI_SLAM_ISAM2OPTIMIZER_H
+
+namespace vi_slam{
+    namespace optimization{
+        class iSAM2Optimizer {
+
+        };
+    }
+}
+
+#endif //VI_SLAM_ISAM2OPTIMIZER_H
---
+++ src/optimization/iSAM2Optimizer.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cit-industry on 11/07/2021.
+//
+
+#include "vi_slam/optimization/iSAM2Optimizer.h"
+
+namespace vi_slam{
+    namespace optimization{
+
+    }
+}
